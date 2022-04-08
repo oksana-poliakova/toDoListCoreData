a77dd155cc0f23c157f7fe6fa958b788fbe15344
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2022-04-08 13:00:31 +0300

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Oksana Poliakova on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
